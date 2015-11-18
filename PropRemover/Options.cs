c453e121ce33bd5764753b50409fb5a0f63d9aff
Egor Aralov <earalov@thumbtack.net>
2015-11-18 21:14:29 +0600

    Add to source control

---
+++ PropRemover/Options.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Debug = UnityEngine.Debug;
+
+namespace PropRemover
+{
+
+    public struct Options
+    {
+        public bool removeSmoke;
+        public bool removeSteam;
+        public bool removeClownHeads;
+        public bool removeIceCones;
+        public bool removeDoughnutSquirrels;
+        public bool removeRandom3dBillboards;
+    }
+
+    public static class OptionsLoader
+    {
+        public static void LoadOptions()
+        {
+            Mod.Options = ModOptions.None;
+            Options options;
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Options));
+                using (StreamReader streamReader = new StreamReader("CSL-PropRemover.xml"))
+                {
+                    options = (Options)xmlSerializer.Deserialize(streamReader);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                options = new Options
+                {
+                    removeSmoke = true,
+                    removeSteam = true,
+                    removeClownHeads = true,
+                    removeIceCones = true,
+                    removeDoughnutSquirrels = true,
+                    removeRandom3dBillboards = true
+                };
+                SaveOptions(options);
+                // No options file yet
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Unexpected " + e.GetType().Name + " loading options: " + e.Message + "\n" + e.StackTrace);
+                return;
+            }
+            if (options.removeSmoke)
+            {
+                Mod.Options |= ModOptions.Smoke;
+            }
+            if (options.removeSteam)
+            {
+                Mod.Options |= ModOptions.Steam; 
+            }
+            if (options.removeClownHeads)
+            {
+                Mod.Options |= ModOptions.ClownHeads;   
+            }
+            if (options.removeIceCones)
+            {
+                Mod.Options |= ModOptions.IceCreamCones;       
+            }
+            if (options.removeDoughnutSquirrels)
+            {
+                Mod.Options |= ModOptions.DoughnutSquirrels;
+            }
+            if (options.removeRandom3dBillboards)
+            {
+                Mod.Options |= ModOptions.Random3DBillboards;        
+            }
+        }
+
+        public static void SaveOptions()
+        {
+            Options options = new Options();
+            if ((Mod.Options & ModOptions.Steam) != 0)
+            {
+                options.removeSmoke = true;
+            }
+            if ((Mod.Options & ModOptions.Smoke) != 0)
+            {
+                options.removeSteam = true;
+            }
+            if ((Mod.Options & ModOptions.ClownHeads) != 0)
+            {
+                options.removeClownHeads = true;
+            }
+            if ((Mod.Options & ModOptions.IceCreamCones) != 0)
+            {
+                options.removeIceCones = true;
+            }
+            if ((Mod.Options & ModOptions.DoughnutSquirrels) != 0)
+            {
+                options.removeDoughnutSquirrels = true;
+            }
+            if ((Mod.Options & ModOptions.Random3DBillboards) != 0)
+            {
+                options.removeRandom3dBillboards = true;
+            }
+            SaveOptions(options);
+        }
+
+        public static void SaveOptions(Options options)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Options));
+                using (StreamWriter streamWriter = new StreamWriter("CSL-PropRemover.xml"))
+                {
+                    xmlSerializer.Serialize(streamWriter, options);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Unexpected " + e.GetType().Name + " saving options: " + e.Message + "\n" + e.StackTrace);
+            }
+        }
+    }
+}
---
+++ PropRemover/PropRemover.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Reflection;
+using ICities;
+using UnityEngine;
+
+namespace PropRemover
+{
+
+    public class PropRemover : MonoBehaviour
+    {
+        private static GameObject removerGo;
+
+        public static void Initialize()
+        {
+            Dispose();
+            removerGo = new GameObject("PropRemover");
+            removerGo.AddComponent<PropRemover>();
+        }
+
+        public static void Dispose()
+        {
+            if (removerGo != null)
+            {
+                Destroy(removerGo);
+                removerGo = null;
+            }
+        }
+
+
+        public void Update()
+        {
+            PropRemover.RemoveProps();
+            Dispose();
+        }
+
+        public static void RemoveProps()
+        {
+            BuildingInfo[] prefabs = Resources.FindObjectsOfTypeAll<BuildingInfo>();
+            for (int j = 0; j < prefabs.Length; j++)
+            {
+                BuildingInfo buildingInfo = prefabs[j];
+                FastList<BuildingInfo.Prop> fastList = new FastList<BuildingInfo.Prop>();
+                if (buildingInfo != null)
+                {
+                    if (buildingInfo.m_props != null)
+                    {
+                        BuildingInfo.Prop[] props = buildingInfo.m_props;
+                        for (int k = 0; k < props.Length; k++)
+                        {
+                            BuildingInfo.Prop prop = props[k];
+                            if (prop != null)
+                            {
+                                if (prop.m_finalProp != null)
+                                {
+                                    if (
+                                        ((Mod.Options & ModOptions.Smoke) == ModOptions.None || !prop.m_finalProp.name.Contains("Smoke") && !prop.m_finalProp.name.Contains("smoke")) &&
+                                        ((Mod.Options & ModOptions.Steam) == ModOptions.None || !prop.m_finalProp.name.Contains("Steam") && !prop.m_finalProp.name.Contains("steam")) &&
+                                        ((Mod.Options & ModOptions.ClownHeads) == ModOptions.None || !prop.m_finalProp.name.Contains("Clown") && !prop.m_finalProp.name.Contains("clown")) &&
+                                        ((Mod.Options & ModOptions.IceCreamCones) == ModOptions.None || !prop.m_finalProp.name.Contains("Cream") && !prop.m_finalProp.name.Contains("cream")) &&
+                                        ((Mod.Options & ModOptions.DoughnutSquirrels) == ModOptions.None || !prop.m_finalProp.name.Contains("Squirrel") && !prop.m_finalProp.name.Contains("squirrel")) &&
+                                        ((Mod.Options & ModOptions.Random3DBillboards) == ModOptions.None || prop.m_finalProp.name != "Billboard_3D_variation")
+                                        )
+                                    {
+                                        fastList.Add(prop);
+                                    }
+                                }
+                                else
+                                {
+                                    fastList.Add(prop);
+                                }
+                            }
+                        }
+                    }
+                    buildingInfo.m_props = fastList.ToArray();
+                }
+            }
+        }
+    }
+}
